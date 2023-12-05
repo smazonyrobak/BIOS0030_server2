15c62ef56880e4fc0c13f2f089535b3cb9dd8c12
Szymon_lic <szymon.lic.22@ucl.ac.uk>
2023-12-05 17:00:32 +0000

    adding different endpoints

--- Flask_web_framework/web_framework.py
+++ Flask_web_framework/web_framework.py
@@ -1,5 +1,5 @@
 from flask import Flask, redirect
-import voila.app
+from voila.app import Voila
 import os
 
 #Flask web framework
@@ -8,11 +8,12 @@ os.chdir(os.path.join(os.getcwd(), '..'))
 uclbiosapp = Flask(__name__)
 
 # Voila configuration
+voila_app = Voila()
 voila_app.initialize(argv=[
     '--no-browser',            # Prevents Voilà from opening a browser automatically
     '--port=8866',             # Sets Voilà to run on port 8866 to avoid conflict with Flask
     '--autoreload=False',      # Sets whether Voilà should autoreload; set to True if needed
-    '--show_tracebacks=False'  # Hides tracebacks in the output
+    '--show_tracebacks=False', # Hides tracebacks in the output
 ])
 @uclbiosapp.route('/')
 def index():
