c5dfd8c1ff5ebd401673b46625c6c64470b015cd
Szymon_lic <szymon.lic.22@ucl.ac.uk>
2023-12-05 16:56:42 +0000

    adding different endpoints

--- Flask_web_framework/web_framework.py
+++ Flask_web_framework/web_framework.py
@@ -3,16 +3,17 @@ import voila.app
 import os
 
 #Flask web framework
+os.chdir(os.path.join(os.getcwd(), '..'))
 
 uclbiosapp = Flask(__name__)
 
 # Voila configuration
-voila_app = voila.app.Voila()
 voila_app.initialize(argv=[
-    '--enable_nbextensions=True',
-    #'--VoilaConfiguration.trust_notebooks=True'
+    '--no-browser',            # Prevents Voilà from opening a browser automatically
+    '--port=8866',             # Sets Voilà to run on port 8866 to avoid conflict with Flask
+    '--autoreload=False',      # Sets whether Voilà should autoreload; set to True if needed
+    '--show_tracebacks=False'  # Hides tracebacks in the output
 ])
-os.chdir(os.path.join(os.getcwd(), '..'))
 @uclbiosapp.route('/')
 def index():
     return redirect('/voila/render/All_tests_launcher/All_tests_launcher.ipynb')
