191698bbe46c193061794e0ccaba78f1cd981dd7
Szymon_lic <szymon.lic.22@ucl.ac.uk>
2023-12-08 13:36:31 +0000

    blabla

--- app.py
+++ app.py
@@ -1,17 +1,12 @@
-import os
 from flask import Flask, redirect
 
 app = Flask(__name__)
 
-root_url = 'https://uclbios-c7accae8eb34.herokuapp.com/'
-
-# Use os.environ.get to access the PORT environment variable provided by Heroku
-port = int(os.environ.get("PORT", 5000))
-
 @app.route('/')
 def home():
-    return redirect(f'{root_url}voila/render/Spatial_Test/Spatial_Test.ipynb')
+    # Redirect to the Voilà-rendered notebook
+    return redirect("https://uclbios-c7accae8eb34.herokuapp.com/voila/render/Spatial_Test/Spatial_Test.ipynb")
 
 if __name__ == '__main__':
-    # Bind the app to the dynamically assigned port
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
