cd30bdd11bc3775e4ee8da769c987ad8ac1afd89
Szymon_lic <szymon.lic.22@ucl.ac.uk>
2023-12-08 14:13:28 +0000

    blabla

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     # Redirect to the Voilà-rendered notebook
-    return redirect("*root link*/voila/render/Spatial_Test/Spatial_Test.ipynb")
+    return redirect("https://uclbios-c7accae8eb34.herokuapp.com/voila/render/Spatial_Test/Spatial_Test.ipynb")
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
