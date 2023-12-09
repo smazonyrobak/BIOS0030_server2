60d20b27734e4544f5790af8381a89e71ed525c3
Szymon_lic <szymon.lic.22@ucl.ac.uk>
2023-12-09 03:15:52 +0000

    last changes

--- flask_framework.py
+++ flask_framework.py
@@ -6,9 +6,11 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return redirect("http://localhost:8866/voila/render/All_tests_launcher/All_tests_launcher.ipynb")
+
 @app.route('/Spatial_Test')
 def spatial():
     return redirect("http://localhost:8866/voila/render/Spatial_Test/Spatial_Test.ipynb")
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
